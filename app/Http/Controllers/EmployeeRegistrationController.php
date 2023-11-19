783bdac12a19ac5d9b0287ed52df7b3bc52efa94
Tahmid Ferdous <tahmid.tf1@gmail.com>
2023-11-19 19:09:44 +0600

    update

--- app/Http/Controllers/EmployeeRegistrationController.php
+++ app/Http/Controllers/EmployeeRegistrationController.php
@@ -221,7 +221,6 @@ class EmployeeRegistrationController extends Controller
         $device_id = $request->get('device_id');
         $APP_KEY = $request->get('APP_KEY');
 
-        return config('app.key');
 
         if (config('app.key') != $APP_KEY) {
             $response = [
